6fd10faed228fc63debf799c079b7fa63ec027f0
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2025-09-10 15:34:32 +0300

    Making the Login Page and link it through export and import in the app route and app config

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Auth} from '../../services/auth';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './login.css'
 })
 export class Login {
+  loginForm: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(private fb: FormBuilder, private authService: Auth) {
+    this.loginForm = this.fb.group({
+      username: ['admin', [Validators.required]],
+      password: ['adminpassword', [Validators.required]]
+    });
+  }
+  onSubmit(): void {
+    if(this.loginForm.valid){
+      this.errorMessage = null;
+      this.authService.login(this.loginForm.value).subscribe({
+        error: (err:any) => {
+          this.errorMessage = err.error?.message || 'Login failed. Please try again.';
+          console.error('Login error:', err);
+        }
+      });
+    }
+  }
 
 }
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
-  
+  private backendUrl = 'https://localhost.8081/api/auth';
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  login(loginData: any): Observable<any> {
+    return this.http.post<any>(`${this.backendUrl}/login`, loginData).pipe(
+      tap(response => {
+        if( response && response.accessToken ) {
+          localStorage.setItem('auth_token', response.accessToken);
+          this.router.navigate(['/']);
+        }
+      })
+    );
+  }
+
+  logout(): void{
+    localStorage.removeItem('auth_token');
+    this.router.navigate(['/login']);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('auth_token');
+  }
+
+  isLoggedIn(): boolean {
+    return this.getToken() !== null;
+  }
+
 }
