f2ee8bcbe1e0372438b076ea5794e9c540c4a951
M-Sayed939 <104098022+M-Sayed939@users.noreply.github.com>
2025-09-10 16:08:22 +0300

    Making the Nav Bar and link it to the auth also added jwt Decode dependency to use it easily

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Router, RouterLink} from '@angular/router';
+import {Auth} from '../../services/auth';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  authService = inject(Auth);
+  router = inject(Router);
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,39 +1,59 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
-import { Router } from '@angular/router';
+import {Injectable, signal} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, tap} from 'rxjs';
+// import { Router } from '@angular/router';
+import {jwtDecode} from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Auth {
   private backendUrl = 'https://localhost.8081/api/auth';
+  isAuthenticated = signal<boolean>(this.hasToken());
 
-  constructor(private http: HttpClient, private router: Router) {
+  constructor(private http: HttpClient) {
   }
 
   login(loginData: any): Observable<any> {
-    return this.http.post<any>(`${this.backendUrl}/login`, loginData).pipe(
-      tap(response => {
-        if( response && response.accessToken ) {
+    return this.http.post(`${this.backendUrl}/login`, loginData).pipe(
+      tap((response: any) => {
+        if (response && response.accessToken) {
           localStorage.setItem('auth_token', response.accessToken);
-          this.router.navigate(['/']);
+          this.isAuthenticated.set(true);
         }
       })
     );
   }
 
-  logout(): void{
+  logout(): void {
     localStorage.removeItem('auth_token');
-    this.router.navigate(['/login']);
+    this.isAuthenticated.set(false);
   }
 
   getToken(): string | null {
     return localStorage.getItem('auth_token');
   }
 
-  isLoggedIn(): boolean {
-    return this.getToken() !== null;
+  // isLoggedIn(): boolean {
+  //   return this.getToken() !== null;
+  // }
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem('auth_token');
   }
 
+  getUsername(): string | null {
+    const token = this.getToken();
+    if (token) {
+      try {
+        const decodedToken: { sub: string } = jwtDecode(token);
+        return decodedToken.sub;
+
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        return null;
+      }
+    }
+    return null;
+  }
 }
